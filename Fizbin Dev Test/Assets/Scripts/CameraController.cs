79268aed6a7f72bb0f3b6a8a6edeb656800860f9
DerPutuck <patrickpfeifer6.pp@gmail.com>
2023-11-09 17:11:07 +0100

    Obstacles and Comments

--- Fizbin Dev Test/Assets/Scripts/CameraController.cs
+++ Fizbin Dev Test/Assets/Scripts/CameraController.cs
@@ -5,17 +5,19 @@ using UnityEngine.Rendering;
 
 public class CameraController : MonoBehaviour
 {
+    [Header("Components")]
     [HideInInspector] public Transform player;
+    
+    [Header("CameraFollow")]
     public float smoothTime;
     private Vector3 velocity = Vector3.zero;
     
-
-    // Update is called once per frame
     void Update()
     {
         MoveCamera();
     }
 
+    // Follows the Player with a given delay (smoothTime) at the distance and angle stated in the "Main Camera" Object 
     void MoveCamera()
     {
         if (player != null)
--- Fizbin Dev Test/Assets/Scripts/PlayerMovementController.cs
+++ Fizbin Dev Test/Assets/Scripts/PlayerMovementController.cs
@@ -6,23 +6,51 @@ using UnityEngine.InputSystem;
 
 public class PlayerMovementController : MonoBehaviour
 {
+    [Header("Components")]
+    private CharacterController charController;
+    
     [Header("Movement")] 
     private Vector2 moveDirection;
     [SerializeField] private float speed;
     
+    [Header("Gravity")]
+    private float verticalVelocity;
+    private float gravity = 9.81f;
+
+    private void Awake()
+    {
+        charController = GetComponent<CharacterController>();
+    }
+
     private void Update()
     {
         MovePlayer();
     }
 
+    private void FixedUpdate()
+    {
+        ApplyGravity();
+    }
+
+    // Checks if the Player wants to move through the Player Input Component on the Player and applies the direction
     public void OnMove(InputAction.CallbackContext context)
     {
         moveDirection = context.ReadValue<Vector2>();
     }
+
+    // Applies Gravity in order to fall down
+    private void ApplyGravity()
+    {
+        if (charController.isGrounded && verticalVelocity < 0)
+            verticalVelocity = 0f;
+        verticalVelocity -= gravity * Time.deltaTime;
+    }
     
+    // Moves the Player in the given direction through the Character Controller on the Player and rotates the Player accordingly
     void MovePlayer()
     {
-        transform.position += new Vector3(moveDirection.x, 0, moveDirection.y) * speed * Time.deltaTime;
+        charController.Move(new Vector3(moveDirection.x, verticalVelocity, moveDirection.y) * speed * Time.deltaTime);
+        
         if (moveDirection != Vector2.zero)
         {
             var rotation = Quaternion.LookRotation(new Vector3(moveDirection.x, 0, moveDirection.y), Vector3.up);
